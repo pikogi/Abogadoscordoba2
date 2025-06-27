3d87ed04a357da7456b806013f11b84bb18f734c
pikogi <giaoliva1@gmail.com>
2025-06-27 14:45:10 -0300

    pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import Link from "next/link"
 import Image from "next/image"
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react"
+import { GoogleTagManager, GoogleTagManagerNoScript } from './components/GoogleTagManager'
+import Script from 'next/script'
 
 import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/header"
@@ -51,33 +53,32 @@ export default function RootLayout({
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
-        
-    {/* Google Tag Manager */}
-    <script
-      dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MX2ZC674');`,
-        }}
-          />
-    {/* End Google Tag Manager */}
+        <GoogleTagManager gtmId="GTM-MX2ZC674" />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1031560682380320');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
-      
-            {/* Google Tag Manager (noscript) */}
-      <noscript>
-     <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-MX2ZC674"
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-    ></iframe>
-     </noscript>
-           {/* End Google Tag Manager (noscript) */}
-
-
+        <GoogleTagManagerNoScript gtmId="GTM-MX2ZC674" />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1031560682380320&ev=PageView&noscript=1"
+          />
+        </noscript>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         <PageLoader>
           <div className="flex flex-col min-h-screen">
